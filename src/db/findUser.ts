94cd82f2b0df54b9fa80f8e7d473b5cd7007ef9c
Gregory Prokhorov <G-Prokhorov@github.com>
2021-06-26 17:15:43 +0300

    fixed the db

--- src/db/findUser.ts
+++ src/db/findUser.ts
@@ -1,7 +1,6 @@
-import UserModel from "./user_db";
+import {userModel} from "./db";
 
 export default async function findUser(username: string) {
-    let userModel = UserModel();
     return await userModel.findOne({
         where: {
             username: username,
