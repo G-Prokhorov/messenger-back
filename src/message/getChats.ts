211b816ea601eb42e6355393746b1ed7a52c534a
G-Prokhorov <grisha.prokhorov03@gmail.com>
2021-07-09 23:20:00 +0300

    add paperclip

--- src/message/getChats.ts
+++ src/message/getChats.ts
@@ -10,8 +10,8 @@ export default async function getChats(username: string, userId: string) {
     let chat: any;
 
     try {
-        chat = await sequelize.query(`SELECT chats.id_chat, u.username, u.name, m.name  AS sender_name, m.username AS sender_username, m.message,  num."numberOfUnread", COALESCE(m."updatedAt", chats."updatedAt") AS lastUpdate FROM chats INNER JOIN users u on u.id = chats.id_user LEFT JOIN
-    (SELECT id_chat, message, username, name, messages."updatedAt" FROM messages INNER JOIN users u on u.id = messages.id_sender WHERE messages.id in (SELECT MAX(id) FROM messages GROUP BY id_chat)) m
+        chat = await sequelize.query(`SELECT chats.id_chat, u.username, u.name, m.name  AS sender_name, m.username AS sender_username, m.message, m.img,  num."numberOfUnread", COALESCE(m."updatedAt", chats."updatedAt") AS lastUpdate FROM chats INNER JOIN users u on u.id = chats.id_user LEFT JOIN
+    (SELECT id_chat, message, username, name, img, messages."updatedAt" FROM messages INNER JOIN users u on u.id = messages.id_sender WHERE messages.id in (SELECT MAX(id) FROM messages GROUP BY id_chat)) m
         on chats.id_chat = m.id_chat INNER JOIN (SELECT id_chat, "numberOfUnread" FROM chats WHERE id_user =${userId}) num on num.id_chat = chats.id_chat WHERE id_user !=${userId}`, {type: QueryTypes.SELECT})
         return chat;
     } catch (e) {
