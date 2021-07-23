9e55c50cff4678ed1e21e2ef5ebbf78c48fc220c
G-Prokhorov <grisha.prokhorov03@gmail.com>
2021-07-23 09:37:11 +0300

    remove

--- messageServ/src/db/findUser.ts
+++
@@ -1,9 +0,0 @@
-import {userModel} from "./db";
-
-export default async function findUser(username: string) {
-    return await userModel.findOne({
-        where: {
-            username: username,
-        },
-    });
-}
