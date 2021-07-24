e1356254332e0a170a91b31d1b12cc943d02d509
G-Prokhorov <grisha.prokhorov03@gmail.com>
2021-07-24 17:29:31 +0300

    temporary save user info in redis

--- gateWay/src/db/findUser.ts
+++ gateWay/src/db/findUser.ts
@@ -1,9 +1,39 @@
 import {userModel} from "./db";
+import redis from "redis";
+
+const client = redis.createClient(6379, process.env.REDIS_HOST);
 
 export default async function findUser(username: string) {
-    return await userModel.findOne({
-        where: {
-            username: username,
-        },
-    });
+    let user: any;
+    try {
+        user = await new Promise((resolve, reject) => {
+            client.get(username, (err, data) => {
+                if (err) {
+                    console.error("Error while get username from redis. " + err);
+                    reject();
+                }
+
+                if (!data) {
+                    reject();
+                }
+
+                resolve(JSON.parse(data));
+            });
+        });
+        client.expire(username, 60 * 10);
+    } catch (e) {
+        console.log("Go to db");
+        user = await userModel.findOne({
+            where: {
+                username: username,
+            },
+        });
+        client.set(username, JSON.stringify(user), "EX", 60 * 10, (err) => {
+            if (err) {
+                console.error("Error while get username from redis. " + err);
+            }
+        });
+    }
+
+    return user;
 }
