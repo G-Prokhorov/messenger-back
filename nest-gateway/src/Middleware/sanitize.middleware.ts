34d52418f3d37d678dfd62d85b69048c6bc80c6e
Gregory Prokhorov <gregory.prokhorov@freshcodeit.com>
2021-08-04 14:00:40 +0300

    add sanitize middleware

---
+++ nest-gateway/src/Middleware/sanitize.middleware.ts
@@ -0,0 +1,25 @@
+import {Injectable, InternalServerErrorException, NestMiddleware} from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import * as sanitizer from "sanitizer";
+
+export function sanitizeMiddleware(req: Request, res: Response, next: NextFunction) {
+        let res1 = sanitizeObj(req.cookies);
+        let res2 = sanitizeObj(req.query);
+        if (!res1 || !res2) {
+            throw new InternalServerErrorException();
+        }
+        next();
+    }
+
+function sanitizeObj(obj):boolean {
+    for (let val in obj) {
+        try {
+            obj[val] = sanitizer.escape(obj[val]);
+        } catch (e) {
+            console.error("Error while sanitize. " + e)
+            return false;
+        }
+    }
+
+    return true;
+}
--- nest-gateway/src/main.ts
+++ nest-gateway/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import {sanitizeMiddleware} from "./Middleware/sanitize.middleware";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.use(sanitizeMiddleware)
   process.on("uncaughtException", (err) => console.error(err))
   await app.listen(3000);
 }
