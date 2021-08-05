16874e79ab68f6646a9f2405614a94758abb0f94
Gregory Prokhorov <gprokhorov@freshcode.org>
2021-08-05 18:17:18 +0300

    message service

--- nest-gateway/src/message/message.controller.ts
+++ nest-gateway/src/message/message.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post, Put} from '@nestjs/common';
+import {Body, Controller, Get, Post, Put, Res} from '@nestjs/common';
 import { MessageService } from './message.service';
 import {ValidationPipe} from "../validation.pipe";
 import {getMessageDto, markReadDto, sendPhotoDto} from "../dto/message.dto";
@@ -8,13 +8,13 @@ export class MessageController {
     constructor(private readonly messageService: MessageService) {}
 
     @Get()
-    getMessage(@Body(new ValidationPipe()) getMessageBody: getMessageDto) {
-        return this.messageService.getMessage(getMessageBody);
+    getMessage(@Body(new ValidationPipe()) getMessageBody: getMessageDto, @Res() res) {
+        return this.messageService.getMessage(getMessageBody, res);
     }
 
     @Put()
-    markRead(@Body(new ValidationPipe()) markReadBody: markReadDto) {
-        return this.messageService.markRead(markReadBody);
+    markRead(@Body(new ValidationPipe()) markReadBody: markReadDto, @Res() res) {
+        return this.messageService.markRead(markReadBody, res);
     }
 
     @Post("/sendPhoto") 
--- nest-gateway/src/message/message.service.ts
+++ nest-gateway/src/message/message.service.ts
@@ -1,14 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import {getMessageInterface, markReadInterface, sendPhotoInterface} from "../interface/message.interface";
+import errorSwitch from "../../../gateWay/src/errorSwitch";
+import lib_PubSub from "../my_library/lib_PubSub";
+import libraryInstance from "../my_library/libraryInstance";
 
 @Injectable()
 export class MessageService {
-    getMessage(body: getMessageInterface) {
-        return body;
+    private pubSub: lib_PubSub = libraryInstance.getInstance();
+
+    getMessage(body: getMessageInterface, res) {
+        const id = this.pubSub.subscribe("resGetMessage", async (err: string, message: string) => {
+            if (err !== 'success') {
+                errorSwitch(res, err);
+                return;
+            }
+            return res.status(200).send(message);
+        });
+
+        this.pubSub.publish("getMessage", {
+            userId: body._userId_,
+            sender: body._userName_,
+            chatId: body.chatId,
+            start: body.start
+        }, id);
     }
 
-    markRead(body: markReadInterface) {
-        return body;
+    markRead(body: markReadInterface, res) {
+        const id = this.pubSub.subscribe("resMarkRead", async (err: string, message: string) => {
+            if (err !== 'success') {
+                errorSwitch(res, err);
+                return;
+            }
+
+            return res.status(200).send(message);
+        });
+
+        this.pubSub.publish("markRead", {
+            userId: body._userId_,
+            sender: body._userName_,
+            chatId: body.chatId,
+            value: body.value
+        }, id);
     }
 
     sendPhoto(body: sendPhotoInterface) {
